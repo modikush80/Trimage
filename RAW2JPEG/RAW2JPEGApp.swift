b544d381b18666e609987bf4940bb82b5a5cb73f
kush Modi <Modikush80@gmail.com>
2026-06-12 15:40:10 -0700

    Initial Commit

---
+++ RAW2JPEG/RAW2JPEGApp.swift
@@ -0,0 +1,17 @@
+//
+//  RAW2JPEGApp.swift
+//  RAW2JPEG
+//
+//  Created by Modi, Kush on 6/12/26.
+//
+
+import SwiftUI
+
+@main
+struct RAW2JPEGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
